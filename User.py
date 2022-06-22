b116ba5903bf75e6580d094157190b4324c98151
Zack <zaxx81@gmail.com>
2022-06-21 22:32:51 -0400

    Updated project to it's own repository

---
+++ User.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self,name,email,dl,age):
+        self.name= name
+        self.email= email
+        self.dl= dl
+        self.age= age
+    
+
+angel= User('Angel','angel@goog.com',1234560,25)
+terrance= User('Terrance','Terrance@goog.com',1234561,30)
+zack= User('Zack','zack@goog.com',1234562,40)
